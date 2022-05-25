2177b5255cf4798b0b06ffa481c127becaa5a55b
Henrique <henriquecrc@al.insper.edu.br>
2022-05-25 13:58:17 -0300

    caveira

--- imagens/codigo_imagem.py
+++ imagens/codigo_imagem.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 WIDTH = 600
 HEIGHT = 600
-tamanhotx = 120
+tamanhotx = 100
 tamanhoty = 110
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Hello World!')
@@ -12,8 +12,8 @@ grama = pygame.image.load('grama.webp').convert()
 grama = pygame.transform.scale(grama, (WIDTH, HEIGHT))
 trator = pygame.image.load('trator_cima.png').convert_alpha()
 trator = pygame.transform.scale(trator, (tamanhotx, tamanhoty))
-pedra = pygame.image.load('pedra.png').convert_alpha()
-pedra = pygame.transform.scale(pedra, (130, 90))
+pedra = pygame.image.load('caveira.png').convert_alpha()
+pedra = pygame.transform.scale(pedra, (100, 100))
 
 while game:
 
