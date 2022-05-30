d20fc169d0659eb91fa5f8fdc4ad48071060b4ac
Jorge <jorgeneto1112@gmail.com>
2022-05-30 16:02:50 -0300

    tela inicio

---
+++ tela inicial.py
@@ -0,0 +1,39 @@
+
+import pygame
+
+pygame.init()
+pygame.mixer.init()
+
+WIDTH = 500
+HEIGHT = 400
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Trator')
+
+game = True
+
+font = pygame.font.SysFont(None, 60)
+text = font.render('Goiania Run', True, (255, 0, 0))
+font2 = pygame.font.SysFont(None, 48)
+text2 = font2.render('Pressione qualquer tecla', True, (255, 255, 255))
+text3 = font2.render('para iniciar o jogo', True, (255, 255, 255))
+image = pygame.image.load('pygame/imagens/trator tela de inicio.jpg').convert()
+image_menor = pygame.transform.scale(image, (500, 400))
+
+pygame.mixer.music.load('pygame/audio/musica sertanejo.mp3')
+pygame.mixer.music.set_volume(0.4)
+pygame.mixer.music.play(loops=-1)
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+    
+    window.fill((0, 0, 0)) 
+    window.blit(image_menor, (0, 0))
+    window.blit(text, (120, 20))
+    window.blit(text2, (40, 300))
+    window.blit(text3, (100, 330))
+    pygame.display.update()  
+
+pygame.quit() 
+
